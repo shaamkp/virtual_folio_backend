367f62d998618c97fa7de1fbc2e17a37ea89a51d
unknown <shyam.steyp@gmail.com>
2021-12-01 11:51:59 +0530

    ajax update but not works

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-    $("a").on("click", "div.done", function(e){
-        e.preventDefault(); 
-        console.log("done");
+$('a').on('click',"li.ajax", function(event){
+    console.log("REDIRECT");
+    event.preventDefault();
+    event.stopPropagation();
+    var targetAttr = $(this).attr('target');
+    var linkHref = $(this).attr('href');
+    
+    setTimeout(function(event){
+      if ( targetAttr === undefined ){
+        alert('redirect in same window');
+        console.log("REDIRECT");
+        window.location.href = linkHref;
+    }
     })
-})
+})   
+
